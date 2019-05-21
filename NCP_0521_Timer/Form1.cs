0c3587e6f3e5ac3882b93b2008ed0968628d414e
kiyeon224 <kiyeon224@naver.com>
2019-05-21 15:34:36 +0900

    #2 타이머 컨트롤과 텍스트박스 컨트롤을 사용하여 카운트 기능 구현

--- NCP_0521_Timer/Form1.cs
+++ NCP_0521_Timer/Form1.cs
@@ -7,16 +7,68 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using Microsoft.VisualBasic;  //입력값이 숫자인지를 확인하는 IsNumeric를 사용하기 위해
+
 namespace NCP_0521_Timer
 {
     public partial class Form1 : Form
     {
+        int CountOrgNum = 0; //초기 카운터
+
+        private bool IntCheck()
+        {
+            if (Information.IsNumeric(this.txtNum.Text))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("숫자를 입력하세요.", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Timer1_Tick(object sender, EventArgs e)
+        private void BtnCount_Click(object sender, EventArgs e)
+        {
+            if (IntCheck())
+            {
+                this.CountOrgNum = Convert.ToInt32(this.txtNum.Text);
+                this.txtNum.ReadOnly = true;
+                this.Timer.Enabled = true;
+            }
+            else
+            {
+                this.txtNum.Text = "";
+                this.txtNum.Focus();
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (this.CountOrgNum < 1)
+            {
+                this.Timer.Enabled = false;
+                this.txtNum.ReadOnly = false;
+                this.txtNum.Text = "";
+                this.txtCountDown.Text = "";
+                MessageBox.Show("펑!", "알림",
+                    MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                this.txtNum.Focus();
+            }
+            else
+            {
+                this.CountOrgNum--;
+                this.txtCountDown.Text = Convert.ToString(this.CountOrgNum);
+            }
+        }
+
+        private void BtnCount_Click_1(object sender, EventArgs e)
         {
 
         }
